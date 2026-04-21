a328b64dfd2e94be3f3d2228a2ce00933035cc9e
Your Name <sadeepadilshan510@gmail.com>
2026-04-21 11:56:58 +0530

    supabase_config.dart updated

---
+++ lib/supabase_config.dart
@@ -0,0 +1,3 @@
+// replace the values below with your Supabase project settings
+const String supabaseUrl = 'https://wqtcbwvbqsxcxlhwbcae.supabase.co';
+const String supabaseAnonKey = 'sb_publishable_2Jud0b0h0PIENQTDmSzNOQ_v_9Uhfgz';
