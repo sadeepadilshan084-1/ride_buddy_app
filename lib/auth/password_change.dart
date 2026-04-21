07b151883b5dc4769ed59cef2023809e8f8d026b
Your Name <sadeepadilshan510@gmail.com>
2026-04-21 11:36:14 +0530

    fix errors auth

--- lib/auth/password_change.dart
+++ lib/auth/password_change.dart
@@ -53,3 +53,4 @@ class PasswordChangedPage extends StatelessWidget {
   }
 }
 
+
--- lib/auth/reset_password.dart
+++ lib/auth/reset_password.dart
@@ -119,7 +119,6 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
                   ),
                 ),
               ],
-
             ),
           ),
         ),
